b718b66460e5411b8424277c3342bb5499a573f2
hangyin <navigator8972@gmail.com>
2019-10-09 17:54:31 +0200

    Add environment variable to try pybullet with customized compilation

--- bullet/env.sh
+++ bullet/env.sh
@@ -12,3 +12,5 @@ export BULLET_PHYSICS_ROOT_DIR=~/workspace/bullet3
 # export LD_LIBRARY_PATH=$BULLET_PHYSICS_ROOT_DIR/build_cmake/src/BulletSoftBody:$LD_LIBRARY_PATH
 # export LD_LIBRARY_PATH=$BULLET_PHYSICS_ROOT_DIR/build_cmake/src/LinearMath:$LD_LIBRARY_PATH
 # export LD_LIBRARY_PATH=$BULLET_PHYSICS_ROOT_DIR/build_cmake/examples/OpenGLWindow:$LD_LIBRARY_PATH
+
+export PYTHONPATH=$BULLET_PHYSICS_ROOT_DIR/build_cmake/examples/pybullet:$PYTHONPATH
