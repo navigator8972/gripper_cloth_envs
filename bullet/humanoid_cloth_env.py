df50e04b626ceba82cbaeea31ae4e64054ce6290
hangyin <navigator8972@gmail.com>
2019-10-10 17:56:26 +0200

    For bullet, add a pybullet environment for a humanoid and a garment. Now it requires customization on python binding and physicsserver to accept setting deformation parameters.

---
+++ bullet/humanoid_cloth_env.py
@@ -0,0 +1,110 @@
+import pybullet as p
+import pybullet_data
+from time import sleep
+import sys
+import os
+
+import numpy as np
+
+from pybullet_envs.robot_bases import MJCFBasedRobot
+
+assets_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../assets')
+
+class MyHumanoid(MJCFBasedRobot):
+
+    def __init__(self, bullet_client, name='humanoid', basePosition=None, baseOrientation=None):
+        MJCFBasedRobot.__init__(self, os.path.join(pybullet_data.getDataPath(), "mjcf",
+                                                'humanoid_symmetric.xml'),name, action_dim=17, obs_dim=44)
+        self._p = bullet_client
+        self.humanoid_objs = bullet_client.loadMJCF(os.path.join(pybullet_data.getDataPath(), "mjcf",
+                                                    'humanoid_symmetric.xml'),
+                                    # flags=p.URDF_USE_SELF_COLLISION |
+                                    # p.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS
+                                    )
+        self.parts, self.jdict, self.ordered_joints, self.robot_body = self.addToScene(bullet_client, self.humanoid_objs)
+        for j in self.ordered_joints:
+            self._p.setJointMotorControl2(self.humanoid_objs[j.bodyIndex], j.jointIndex, p.POSITION_CONTROL,
+                                            targetVelocity=0,
+                                            positionGain=1000,
+                                            velocityGain=50,
+                                            maxVelocity=1,
+                                            force=100)
+        
+        if basePosition is not None and baseOrientation is not None:
+            self.reset_pose(basePosition, baseOrientation)
+        
+        #set arm inital pose
+        self.jdict['left_shoulder1'].set_state(np.pi/6, 0)
+        self.jdict['left_shoulder2'].set_state(np.pi/6, 0)
+        self.jdict['right_shoulder1'].set_state(-np.pi/6, 0)
+        self.jdict['right_shoulder2'].set_state(-np.pi/6, 0)
+
+        self.jdict['left_shoulder1'].set_position(np.pi/6)
+        self.jdict['left_shoulder2'].set_position(np.pi/6)
+        self.jdict['right_shoulder1'].set_position(-np.pi/6)
+        self.jdict['right_shoulder2'].set_position(-np.pi/6)
+
+
+        self.fixConstraintId = None
+        return
+
+    def fixBase(self):
+        basePose = self.parts[self.robot_name].get_pose()
+        # print(basePose)
+        self.fixConstraintId = self._p.createConstraint(self.humanoid_objs[0], -1, -1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], basePose[:3])
+        return
+    
+    def __del__(self):
+        # MJCFBasedRobot.__del__()
+        if self.fixConstraintId is not None:
+            self._p.removeConstraint(self.fixConstraintId)
+        return
+
+
+def main():
+    physicsClient = p.connect(p.GUI)
+
+
+    p.setGravity(0, 0, -10)
+
+    #data path to search object meshes
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+    # franka = p.loadURDF(fileName=os.path.join(assets_path, 'franka/robots/panda_arm_hand_mrp.urdf'), basePosition=[0, 0, 0], useFixedBase=True, useMaximalCoordinates=True)
+    plane = p.loadURDF("plane.urdf")
+    # cloth = p.loadSoftBody(fileName=os.path.join(assets_path, 'tshirt.obj'), basePosition=[-0.5, -0.5, 1.2], scale=1)
+
+    cloth = p.loadSoftBody(fileName=os.path.join(assets_path, 'tshirt.obj'), basePosition=[0, 0, 1.3], baseOrientation=[ 0, 0, 0.7071068, 0.7071068 ], scale=1, 
+        collisionMargin=0.05, springElasticStiffness=1, springDampingStiffness=0.1)
+    cloth_mesh = p.getMeshData(cloth)
+
+    
+    humanoid = MyHumanoid(p)
+    humanoid.fixBase()
+
+ 
+    runSimulation = False
+    useRealTimeSimulation = 0
+
+    # cubeId = p.loadURDF("cube_small.urdf", 0, 0, 1)
+    # cid = p.createConstraint(cubeId, -1, -1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0, 0, 1])
+
+
+    if (useRealTimeSimulation):
+        p.setRealTimeSimulation(1)
+
+    while p.isConnected():
+        
+
+        keys = p.getKeyboardEvents()
+        if ord('q') in keys and keys[ord('q')]&p.KEY_WAS_TRIGGERED:
+            break
+        if ord(' ') in keys and keys[ord(' ')]&p.KEY_WAS_TRIGGERED:
+            runSimulation = not runSimulation
+        if runSimulation:
+            p.stepSimulation()
+    # p.removeConstraint(cid)
+    return
+
+if __name__ == "__main__":
+    main()     
